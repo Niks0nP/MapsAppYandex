39bb49346c525324d2d4e751873b8e70adf4ba9b
Niks0nP <nikita.niko40@gmail.com>
2024-09-30 18:28:38 +0300

    add_points_on_map

--- app/src/main/java/com/example/mapsappyandex/MainActivity.kt
+++ app/src/main/java/com/example/mapsappyandex/MainActivity.kt
@@ -6,16 +6,22 @@ import android.location.Location
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.mapsappyandex.databinding.ActivityMainBinding
 import com.google.android.gms.location.LocationServices
 import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKit
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.InputListener
+import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.map.MapObjectTapListener
 import com.yandex.mapkit.mapview.MapView
 import com.yandex.mapkit.traffic.TrafficLayer
+import com.yandex.runtime.image.ImageProvider
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         MapKitFactory.setApiKey(BuildConfig.API_KEY)
         MapKitFactory.initialize(this)
@@ -31,23 +38,62 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        enableEdgeToEdge()
+        locationPermission.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
 
         mapView = binding.mapView
         val mapKit = MapKitFactory.getInstance()
-
-        val locationOnMapUser = mapKit.createUserLocationLayer(mapView.mapWindow)
-        locationOnMapUser.isVisible = true
-
         val trafficLayer = mapKit.createTrafficLayer(mapView.mapWindow)
 
         binding.button.setOnClickListener{
             showTraffic(trafficLayer, binding)
         }
 
-        binding.buttonUserLocation.setOnClickListener{
+        binding.showUserLocation.setOnClickListener{
             getCurrentLocation()
         }
+
+        mapView.mapWindow.map.addInputListener(clickProcessingOnMap())
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+
+    override fun onStop() {
+        mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    private val locationPermission = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                getCurrentLocation()
+                val mapKit = MapKitFactory.getInstance()
+                showUserOnMap(mapKit)
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                getCurrentLocation()
+            } else -> {
+            Toast.makeText(this, R.string.location_permission_denied, Toast.LENGTH_SHORT).show()
+        }
+        }
+    }
+
+    private fun showUserOnMap(mapKit: MapKit) {
+        val locationOnMapUser = mapKit.createUserLocationLayer(mapView.mapWindow)
+        locationOnMapUser.isVisible = true
     }
 
     private fun getCurrentLocation() {
@@ -67,7 +113,7 @@ class MainActivity : AppCompatActivity() {
                 val currentLocation = Point(location.latitude, location.longitude)
 
                 mapView.mapWindow.map.move(
-                    CameraPosition(currentLocation, 12.0f, 0.0f, 0.0f),
+                    CameraPosition(currentLocation, 15.0f, 0.0f, 0.0f),
                     Animation(Animation.Type.SMOOTH, 1f), null
                 )
             } else
@@ -77,23 +123,6 @@ class MainActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT)
                     .show()
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mapView.onStart()
-        MapKitFactory.getInstance().onStart()
-    }
-
-    override fun onStop() {
-        mapView.onStop()
-        MapKitFactory.getInstance().onStop()
-        super.onStop()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        getCurrentLocation()
     }
 
     private fun showTraffic(probity: TrafficLayer, binding: ActivityMainBinding) {
@@ -106,18 +135,33 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 1) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getCurrentLocation()
-            }
-            else
-                Toast.makeText(this, R.string.location_permission_denied, Toast.LENGTH_SHORT).show()
+    private fun showPoint(point: Point) {
+        val imageProvider = ImageProvider.fromResource(this, R.drawable.point_location)
+        val placemark = mapView.mapWindow.map.mapObjects.addPlacemark().apply {
+            geometry = Point(point.latitude, point.longitude)
+            setIcon(imageProvider)
+            Toast.makeText(this@MainActivity, "Точка добавлена", Toast.LENGTH_SHORT).show()
         }
+
+        val tapListener = MapObjectTapListener { _, _ ->
+            Toast.makeText(
+                this@MainActivity,
+                "Tapped the point (${point.longitude}, ${point.latitude})",
+                Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        placemark.addTapListener(tapListener)
+    }
+
+    private fun clickProcessingOnMap()  = object : InputListener {
+            override fun onMapTap(p0: Map, p1: Point) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onMapLongTap(p0: Map, p1: Point) {
+                showPoint(p1)
+            }
+
     }
 }
